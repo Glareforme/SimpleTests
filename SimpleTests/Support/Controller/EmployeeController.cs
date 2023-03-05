80271c4fe01a666b75752859289a8887742be2e8
Glareforme <91750407+Glareforme@users.noreply.github.com>
2023-03-05 20:07:34 +0200

    change project struct

--- SimpleTests/Support/Controller/EmployeeController.cs
+++ SimpleTests/Support/Controller/EmployeeController.cs
@@ -1,5 +1,6 @@
 ﻿using SimpleTests.Drivers;
 using SimpleTests.Support.Constants;
+using SimpleTests.Support.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,22 +21,42 @@ namespace SimpleTests.Support.Controller
 
         public async Task<string> GetEmployeesAsync()
         {
-            var response = await apiRequestHelper.GetAsync(BaseConstants.GetEmployeeUrl);
-            if (response.IsSuccessStatusCode)
-            {
-                return response.Content.ToString();
-            }
-            else throw new Exception($"Request failed with status code {response.StatusCode}");
+            var response = await apiRequestHelper.GetAsync(APITestendpoints.GetEmployeeUrl);
+
+            return ReturnResponseContent(response);
         }
 
         public async Task<string> GetEmployeeByIdAsync(string employeeId)
         {
-            var response = await apiRequestHelper.GetAsync(string.Join(BaseConstants.GetEmployeeByIdUrl, employeeId));
-            if (response.IsSuccessStatusCode)
-            {
-                return response.Content.ToString();
-            }
-            else throw new Exception($"Request failed with status code {response.StatusCode}");
+            var response = await apiRequestHelper.GetAsync(string.Join(APITestendpoints.GetEmployeeByIdUrl, employeeId));
+
+            return ReturnResponseContent(response);
+        }
+
+        public async Task<string> AddEmployeeAsync(EmployeeModel employee)
+        {
+            var response = await apiRequestHelper.PostAsync(APITestendpoints.PostCreateEmployeeUrl, employee);
+
+            return ReturnResponseContent(response);
+        }
+
+        public async Task<string> UpdateEmployeeById(string employeeId, EmployeeModel newEmployeeData)
+        {
+            var response = await apiRequestHelper.PutAsync(string.Join(APITestendpoints.PutUpdateEmployeeByIdUrl, employeeId), newEmployeeData);
+
+            return ReturnResponseContent(response);
+        }
+
+        public async Task<string> DeleteEmployeeById(string employeeId)
+        {
+            var response = await apiRequestHelper.DeleteAsync(string.Join(APITestendpoints.DeleteEmployeeUrl, employeeId));
+
+            return ReturnResponseContent(response);
+        }
+
+        private static string ReturnResponseContent(HttpResponseMessage response)
+        {
+            return response.IsSuccessStatusCode ? response.Content.ToString() : throw new Exception($"Request failed with status code {response.StatusCode}");
         }
     }
 }
---
+++ SimpleTests/Support/Models/EmployeeModel.cs
@@ -0,0 +1,11 @@
+﻿namespace SimpleTests.Support.Models
+{
+    public class EmployeeModel
+    {
+        public string Name { get; set; }
+
+        public string Salary { get; set; }
+
+        public string Age { get; set; }
+    }
+}
